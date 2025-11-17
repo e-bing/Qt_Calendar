8a6556f14b91cd5726619328e334f273c60ba74c
Yebin <ebing.code@gmail.com>
2025-11-17 11:38:14 +0900

    레포지토리 정리

--- Calendar/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Calendar_FILE D:/work/LQT6/Calendar/Calendar/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/Calendar.exe)
-set(__QT_DEPLOY_TARGET_Calendar_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Calendar_RUNTIME_DLLS C:/Qt/6.10.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/mingw_64/bin/Qt6Core.dll)
