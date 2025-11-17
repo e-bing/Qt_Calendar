a2fcca64d550e2b1799cd4bfa0198533465390a0
Yebin <ebing.code@gmail.com>
2025-11-17 17:43:11 +0900

    ScheduleManager 헤더 파일 내용 추가

--- models/schedulemanager.h
+++ models/schedulemanager.h
@@ -11,7 +11,35 @@ class ScheduleManager : public QObject
 {
     Q_OBJECT
 public:
-    ScheduleManager();
+    explicit ScheduleManager(const QString& dbPath, QObject* parent = nullptr);
+    ~ScheduleManager();
+
+    bool openDatabase();
+    void closeDatabase();
+
+    // 일정 추가
+    bool addSchedule(const Schedule& schedule);
+
+    // 일정 수정
+    bool updateSchedule(const Schedule& schedule);
+
+    // 일정 삭제
+    bool deleteSchedule(int id);
+
+    // 전체 일정 조회
+    QList<Schedule> getAllSchedules();
+
+    // 일정 검색
+    QList<Schedule> searchSchedules(const QString& keyword);
+
+signals:
+    void databaseError(const QString& error);
+
+private:
+    QSqlDatabase m_database;
+    QString m_dbPath;
+
+    Schedule scheduleFromQuery(const QVariantMap& record);
 };
 
 #endif // SCHEDULEMANAGER_H
