72f92dd1dbf08dbb6d5a1a2d491aef99facaa453
gcmoon135 <gcmoon13@kakao.com>
2025-11-18 14:04:13 +0900

    카테고리 폼/매니저 작업
    코드 및 DB연동 완료

--- models/category.cpp
+++ models/category.cpp
@@ -1,6 +1,6 @@
 #include "category.h"
 
-Category::Category() : m_id(-1)
+Category::Category() : m_id(-1)     // 생성자 초기화 리스트
 {}
 
 Category::Category(int id, const QString& title, const QString& color) : m_id(id), m_title(title), m_color(color)
@@ -8,17 +8,17 @@ Category::Category(int id, const QString& title, const QString& color) : m_id(id
 }
 
 // getter functions
-int Category::id()
+int Category::id() const
 {
     return m_id;
 }
 
-QString Category::title()
+QString Category::title() const
 {
     return m_title;
 }
 
-QString Category::color()
+QString Category::color() const
 {
     return m_color;
 }
